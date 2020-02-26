3a86c0b5107100e08de021d6fa137907a223bb5e
Felix Boquet <boquet@brocelia.fr>
2020-02-26 10:57:51 +0100

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Felix Boquet on 26/02/2020.
+//  Copyright © 2020 Felix Boquet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
